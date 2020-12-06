badf816c0e3b162378e53c0a1cf800baeb40187f
Elyse Wells <cewells89@gmail.com>
2020-12-05 23:01:27 -0500

    added post route to api routes

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ Express Note Taker App
 
 
 
-https://expressjs.com/en/starter/static-files.html
+using path package to reference files in a directory other than the one the server.js file is in. https://expressjs.com/en/starter/static-files.html
+
+
+increment IDs for records inserted: https://stackoverflow.com/questions/62926005/add-an-incrementing-id-property-to-each-object-in-array-after-it-has-been-submit
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,6 +12,22 @@ const notesData = require("../db/db");
 module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
         res.json(notesData);
-      });
+    });
+    
+    app.post("/api/notes", function(req, res) {
+        const newNote = req.body;
+
+        notesData.push(newNote);
+
+        // Increments the ID of each note that is inserted.
+        notesData.forEach((note,index) => {
+            note.id = index +1;
+        });
+
+        console.log(newNote);
+          
+        res.json(newNote);
+    });
+
     
 };
